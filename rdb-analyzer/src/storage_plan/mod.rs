db641ace41df24ab023a0089a1423b7fc422bef3
losfair <zhy20000919@hotmail.com>
2021-06-16 13:45:44 +0800

    Planner test

--- rdb-analyzer/src/storage_plan/mod.rs
+++ rdb-analyzer/src/storage_plan/mod.rs
@@ -1,8 +1,11 @@
-use std::{collections::BTreeMap, sync::Arc};
+use std::{collections::BTreeMap, fmt::Display, sync::Arc};
 
 use crate::schema::compile::FieldType;
 
 pub mod planner;
+
+#[cfg(test)]
+mod planner_test;
 
 pub type StorageKey = [u8; 16];
 
@@ -22,4 +25,69 @@ pub struct StorageNode {
 pub enum StorageNodeKey {
   Const(StorageKey),
   Set(Box<StorageNode>),
+}
+
+impl Display for StorageNodeKey {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      StorageNodeKey::Const(x) => write!(f, "{}", hex::encode(x)),
+      StorageNodeKey::Set(_) => write!(f, "set_key"),
+    }
+  }
+}
+
+impl Display for StoragePlan {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    for (node_name, node) in &self.nodes {
+      write!(f, "top-level node: {}{}", node_name, node)?;
+    }
+    Ok(())
+  }
+}
+
+impl StorageNode {
+  fn display_fmt(&self, indent: usize, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(
+      f,
+      " {} {}{}",
+      self.ty,
+      self
+        .key
+        .as_ref()
+        .map(|x| format!("{}", x))
+        .unwrap_or_else(|| "no_key".to_string()),
+      if self.subspace_reference {
+        " subspace_reference"
+      } else {
+        ""
+      }
+    )?;
+    write!(f, "\n")?;
+
+    match &self.key {
+      Some(StorageNodeKey::Set(x)) => {
+        for _ in 0..indent + 1 {
+          write!(f, ".")?;
+        }
+        write!(f, "<set_member>")?;
+        x.display_fmt(indent + 1, f)?;
+      }
+      _ => {
+        for (child_name, child_node) in &self.children {
+          for _ in 0..indent + 1 {
+            write!(f, ".")?;
+          }
+          write!(f, "{}", child_name)?;
+          child_node.display_fmt(indent + 1, f)?;
+        }
+      }
+    }
+    Ok(())
+  }
+}
+
+impl Display for StorageNode {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    self.display_fmt(0, f)
+  }
 }
---
+++ rdb-analyzer/src/storage_plan/planner_test.rs
@@ -0,0 +1,44 @@
+use bumpalo::Bump;
+
+use crate::schema::{compile::compile, grammar::parse};
+
+use super::planner::generate_plan_for_schema;
+
+#[test]
+fn test_planner_simple() {
+  let alloc = Bump::new();
+  let ast = parse(
+    &alloc,
+    r#"
+    type Item<T> {
+      @packed inner: T,
+      something_else: string,
+    }
+    type Duration<T> {
+      start: T,
+      end: T,
+    }
+    type Recursive<T> {
+      inner: Recursive<T>?,
+    }
+    type BinaryTree<T> {
+      left: BinaryTree<T>?,
+      right: BinaryTree<T>?,
+      value: T?,
+    }
+    type InternalSet {
+      s: set<int64>,
+    }
+    export set<Item<Duration<int64>>> items;
+    export Recursive<int64> item;
+    export BinaryTree<int64> a_binary_tree;
+    export set<BinaryTree<int64>> many_binary_trees;
+    export InternalSet an_internal_set;
+    export set<InternalSet> nested_internal_sets;
+  "#,
+  )
+  .unwrap();
+  let output = compile(&ast).unwrap();
+  let plan = generate_plan_for_schema(&output).unwrap();
+  println!("{}", plan);
+}
