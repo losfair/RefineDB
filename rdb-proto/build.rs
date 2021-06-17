de6880e25ae41224977fe5ee707629e74e90286a
losfair <zhy20000919@hotmail.com>
2021-06-17 12:08:42 +0800

    Fix rerun-if-changed

--- rdb-proto/build.rs
+++ rdb-proto/build.rs
@@ -1,6 +1,6 @@
 fn main() {
   let proto_root = "src/protos";
-  println!("cargo:rerun-if-changed={}", proto_root);
+  println!("cargo:rerun-if-changed={}/rdbrpc.proto", proto_root);
   protoc_grpcio::compile_grpc_protos(&["rdbrpc.proto"], &[proto_root], &proto_root, None)
     .expect("Failed to compile gRPC definitions!");
 }
