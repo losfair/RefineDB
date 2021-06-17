20230507e7bdad73428893e00d358d482bccf054
losfair <zhy20000919@hotmail.com>
2021-06-17 11:57:52 +0800

    Format

--- rdb-server/build.rs
+++ rdb-server/build.rs
@@ -1,10 +1,6 @@
 fn main() {
   let proto_root = "src/protos";
   println!("cargo:rerun-if-changed={}", proto_root);
-  protoc_grpcio::compile_grpc_protos(
-      &["rdbrpc.proto"],
-      &[proto_root],
-      &proto_root,
-      None
-  ).expect("Failed to compile gRPC definitions!");
+  protoc_grpcio::compile_grpc_protos(&["rdbrpc.proto"], &[proto_root], &proto_root, None)
+    .expect("Failed to compile gRPC definitions!");
 }
