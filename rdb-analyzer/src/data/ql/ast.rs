2a9c835c7fef0f1118e9a7c53910171c05e75bb9
losfair <zhy20000919@hotmail.com>
2021-06-20 13:00:02 +0800

    RefineQL updates

--- rdb-analyzer/src/data/ql/ast.rs
+++ rdb-analyzer/src/data/ql/ast.rs
@@ -6,6 +6,41 @@ pub struct QlRoot<'a> {
 
 pub struct QlGraph<'a> {
   pub name: &'a str,
+  pub params: Vec<'a, (&'a str, QlType<'a>)>,
+  pub return_type: Option<QlType<'a>>,
+  pub stmts: Vec<'a, QlStmt<'a>>,
+}
+
+pub struct QlStmt<'a> {
+  pub location: usize,
+  pub kind: QlStmtKind<'a>,
+}
+
+pub enum QlStmtKind<'a> {
+  Let { name: &'a str, value: QlExpr<'a> },
+  Assign { base: QlExpr<'a>, value: QlExpr<'a> },
+  EffectAssign { base: QlExpr<'a>, value: QlExpr<'a> },
+}
+
+pub struct QlExpr<'a> {
+  pub location_start: usize,
+  pub location_end: usize,
+  pub kind: QlExprKind<'a>,
+}
+
+pub struct QlType<'a> {
+  pub name: &'a str,
+  pub params: Vec<'a, QlType<'a>>,
+}
+
+pub enum QlExprKind<'a> {
+  Primitive(Literal<'a>),
+  ValueRef(&'a str),
+  GetField(&'a QlExpr<'a>, &'a str),
+  NewMap(Vec<'a, (&'a str, QlExpr<'a>)>),
+  UnwrapOptional(&'a QlExpr<'a>),
+  BuildTable(QlType<'a>, &'a QlExpr<'a>),
+  GetSetElement(&'a QlExpr<'a>, &'a str, &'a QlExpr<'a>),
 }
 
 pub enum Literal<'a> {
