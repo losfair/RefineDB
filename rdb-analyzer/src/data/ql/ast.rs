5b8a0a29f1aa055ebcf1529fde319f1d707f2490
losfair <zhy20000919@hotmail.com>
2021-06-20 11:53:37 +0800

    Init QL

---
+++ rdb-analyzer/src/data/ql/ast.rs
@@ -0,0 +1,14 @@
+use bumpalo::collections::vec::Vec;
+
+pub struct QlRoot<'a> {
+  pub graphs: Vec<'a, QlGraph<'a>>,
+}
+
+pub struct QlGraph<'a> {
+  pub name: &'a str,
+}
+
+pub enum Literal<'a> {
+  Integer(i64),
+  String(&'a str),
+}
--- rdb-analyzer/src/data/ql/mod.rs
+++ rdb-analyzer/src/data/ql/mod.rs
@@ -1,1 +1,14 @@
+use lalrpop_util::lalrpop_mod;
 
+pub mod ast;
+mod state;
+
+lalrpop_mod!(pub language, "/data/ql/language.rs");
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum QlError {
+  #[error("invalid literal")]
+  InvalidLiteral,
+}
