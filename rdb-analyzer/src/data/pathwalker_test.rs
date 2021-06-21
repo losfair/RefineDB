ab332c0548fe830d22944fa65243cb96d275db6c
losfair <zhy20000919@hotmail.com>
2021-06-21 15:23:23 +0800

    print_path_examples

--- rdb-analyzer/src/data/pathwalker_test.rs
+++ rdb-analyzer/src/data/pathwalker_test.rs
@@ -1,12 +1,74 @@
+use std::{collections::HashSet, sync::Arc};
+
 use bumpalo::Bump;
 
 use crate::{
   data::value::PrimitiveValue,
-  schema::{compile::compile, grammar::parse},
-  storage_plan::{planner::generate_plan_for_schema, StoragePlan},
+  schema::{
+    compile::{compile, CompiledSchema, FieldAnnotationList, FieldType},
+    grammar::parse,
+  },
+  storage_plan::{planner::generate_plan_for_schema, StorageNode, StoragePlan},
 };
 
 use super::pathwalker::PathWalker;
+
+fn print_path_examples(
+  schema: &CompiledSchema,
+  field: &FieldType,
+  node: &StorageNode,
+  walker: Arc<PathWalker>,
+  path: &String,
+  recursion_set: &mut HashSet<usize>,
+) {
+  match field {
+    FieldType::Table(x) => {
+      println!("{} -> {}", path, walker.generate_key_pretty());
+      let specialized_ty = schema.types.get(x).unwrap();
+      for (name, (field, _)) in &specialized_ty.fields {
+        if recursion_set.contains(&(field as *const _ as usize)) {
+          continue;
+        }
+        recursion_set.insert(field as *const _ as usize);
+        let path = format!("{}.{}", path, name);
+        let walker = walker.enter_field(&**name).unwrap();
+        let node = walker.node();
+        print_path_examples(schema, field, node, walker, &path, recursion_set);
+        recursion_set.remove(&(field as *const _ as usize));
+      }
+    }
+    FieldType::Primitive(_) => {
+      println!("{} -> {}", path, walker.generate_key_pretty());
+    }
+    FieldType::Set(ty) => {
+      let specialized_ty = match &**ty {
+        FieldType::Table(x) => schema.types.get(x).unwrap(),
+        _ => unreachable!(),
+      };
+      let (primary_key_name, (primary_key_ty, _)) = specialized_ty
+        .fields
+        .iter()
+        .find(|(_, (_, ann))| ann.as_slice().is_primary())
+        .unwrap();
+      let primary_key_example = match primary_key_ty {
+        FieldType::Primitive(x) => PrimitiveValue::example_value_for_type(*x),
+        _ => unreachable!(),
+      };
+      let walker = walker.enter_set(&primary_key_example).unwrap();
+      let path = format!(
+        "{}[{} == {:?}]",
+        path, primary_key_name, primary_key_example
+      );
+      let node = node.set.as_ref().unwrap();
+      println!("{} -> {}", path, walker.generate_key_pretty());
+      print_path_examples(schema, &**ty, &**node, walker, &path, recursion_set);
+    }
+    FieldType::Optional(x) => {
+      let path = format!("{}!", path);
+      print_path_examples(schema, &**x, node, walker, &path, recursion_set);
+    }
+  }
+}
 
 #[test]
 fn basic() {
@@ -130,4 +192,14 @@ fn basic() {
       .len(),
     54
   );
+  for (export_name, export_ty) in &schema.exports {
+    print_path_examples(
+      &schema,
+      export_ty,
+      plan.nodes.get(&**export_name).unwrap(),
+      PathWalker::from_export(&plan, &**export_name).unwrap(),
+      &format!("{}", export_name),
+      &mut HashSet::new(),
+    );
+  }
 }
